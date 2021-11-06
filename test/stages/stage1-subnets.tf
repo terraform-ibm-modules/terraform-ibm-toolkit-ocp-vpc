0d61c3e0b6cb568a9b9d57bda6b9b3db72893847
Sean Sundberg <seansund@us.ibm.com>
2021-11-06 08:21:22 -0500

    Updates handling of provider config
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-subnets.tf
+++ test/stages/stage1-subnets.tf
@@ -3,7 +3,6 @@ module "subnets" {
 
   resource_group_id = module.resource_group.id
   region            = var.region
-  ibmcloud_api_key  = var.ibmcloud_api_key
   vpc_name          = module.vpc.name
   gateways          = module.gateways.gateways
   _count            = 2
