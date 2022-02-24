fbea5942d22bd62be6cc0ce49f9e552782f0c5c6
Sean Sundberg <seansund@us.ibm.com>
2022-02-24 00:03:38 -0600

    Updates test variables (#57)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-gateways.tf
+++ test/stages/stage1-gateways.tf
@@ -4,5 +4,4 @@ module "gateways" {
   resource_group_id = module.resource_group.id
   region            = var.region
   vpc_name          = module.vpc.name
-  subnet_count      = var.vpc_subnet_count
 }
