4beeeb32a29101b83d29abae1fd524874276977d
Tim Robinson <timroster@gmail.com>
2022-03-16 12:13:02 -0700

    extend name_prefix for ci concurrency
    Signed-off-by: Tim Robinson <timroster@gmail.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -5,5 +5,19 @@ terraform {
     ibm = {
       source = "ibm-cloud/ibm"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
   }
+}
+
+locals {
+  name_prefix_test = "${var.name_prefix}-${random_string.this.result}"
+}
+
+resource "random_string" "this" {
+  length = 6
+  special = false
+  upper = false
 }
